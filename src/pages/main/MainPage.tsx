747495ccd94adbb2b8f2579faeef29cf0e992b09
Dmitriy Latyshev <dislaty@mail.ru>
2024-06-01 17:36:26 +0200

    feat: добавили alias

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,3 @@
+export function MainPage() {
+    return <p>Страничка</p>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@helpers': path.resolve(__dirname, './src/helpers'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@api': path.resolve(__dirname, './src/api'),
+        },
+    },
     plugins: [react()],
 });
