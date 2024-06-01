c8cfc499cadbbbbaff52848fb1a783845b9a64b6
Dmitriy Latyshev <dislaty@mail.ru>
2024-06-01 17:55:33 +0200

    feat: добавили store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { MainPage } from '@pages/main';
+import { RootState } from '@store/store';
+import { increment } from '@store/counterSlice';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
 
     return (
         <>
@@ -24,7 +27,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => dispatch(increment())}>
                     count is {count}
                 </button>
                 <p>
