3bc87acdbf214c06c0239d30d0fcb9cb26dde118
tiutenok <alekseitiutenkov@mail.ru>
2024-06-24 10:34:20 +0300

    Add component MovieRating

--- src/components/MovieInfoBlock/MovieInfoBlock.tsx
+++ src/components/MovieInfoBlock/MovieInfoBlock.tsx
@@ -1,27 +1,13 @@
 import { MovieDescription } from '@components/MovieDescription';
-import { StarRating } from '@components/StarRating';
-import { useRateMovie } from '@hooks/useRateMovie';
-import { useAppSelector } from '@store/store';
+import { MovieRating } from '@components/MovieRating';
 
 import styles from './MovieInfoBlock.module.css';
 
 export const MovieInfoBlock = () => {
-    const [initialRating, rateMovie] = useRateMovie();
-
-    const isAuthorized = useAppSelector(
-        (state) => state.authorization.isAuthorized
-    );
-
     return (
         <div className={styles.container}>
             <MovieDescription />
-            {isAuthorized && (
-                <StarRating
-                    defaultTextLabel="Оцените фильм"
-                    onRatingChange={rateMovie}
-                    initialRating={initialRating}
-                />
-            )}
+            <MovieRating />
         </div>
     );
 };
